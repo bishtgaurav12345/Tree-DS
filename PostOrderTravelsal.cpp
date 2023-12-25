521142685142e0b8723ec7d4053c21bc22a8c5c4
gaurav bisht <gauravengg.bisht@gmail.com>
2023-12-25 12:48:21 +0530

    DFS 3 code added

---
+++ PostOrderTravelsal.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+
+struct node {
+  int data;
+  node *left, *right;
+  node(int d){
+    data= d;
+    left=right=NULL;
+  };
+};
+
+
+void printTree(node *root){
+  if(root ==NULL){
+    return;
+  }
+  printTree(root->left);
+  printTree(root->right);
+  cout<<root->data<<endl;
+}
+
+
+int main() 
+{
+  node *root = new node(1);
+  root->left= new node(2);
+  root->right= new node(3);
+  root->left->left = new node(4);
+  root->left->right = new node(5);
+  root->right->left = new node(6);
+  root->right->right = new node(7);
+  
+  printTree(root);
+}
